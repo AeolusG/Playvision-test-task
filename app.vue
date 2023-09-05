86ebea87c778f7eeefaad517915302d2bff22701
Olga <olga68ko@gmail.com>
2023-09-05 21:16:05 +0300

    make reusable modal window, add example

--- app.vue
+++ app.vue
@@ -1,5 +1,74 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <TheModalWindow
+    :show="isShown"
+    :typeOfClosing="closeType"
+    :title="anotherTitle"
+  >
+    <template v-slot:close>
+      <div class="timer">
+        {{ currentTime }}
+      </div>
+    </template>
+    <template v-slot:content>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </div>
+    </template>
+  </TheModalWindow>
+  <button @click="changeVisibility">показать окно</button>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      isShown: false,
+      closeType: 'none',
+      currentTime: null,
+      timer: null,
+      anotherTitle: 'Divide et impera',
+    };
+  },
+
+  methods: {
+    changeVisibility() {
+      this.isShown = true;
+      if (this.closeType === 'timer') {
+        this.startTimer();
+      }
+    },
+    startTimer() {
+      this.currentTime = 6;
+      this.timer = setInterval(() => {
+        this.currentTime--;
+      }, 1000);
+    },
+    stopTimer() {
+      clearTimeout(this.timer);
+    },
+  },
+  watch: {
+    currentTime(time) {
+      if (time === 0) {
+        this.stopTimer();
+        this.isShown = false;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.timer {
+  top: 0%;
+  position: absolute;
+  left: 85%;
+  font-size: 20px;
+}
+.content {
+  padding: 10px;
+}
+</style>
---
+++ components/TheModalWindow.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="popup" v-if="show">
+    <div class="popup__content">
+      <header class="popup__title">
+        <h2>{{ title }}</h2>
+        <slot name="close" v-if="typeOfClosing === 'timer'"></slot>
+        <slot name="close" v-if="typeOfClosing === 'cross'"></slot>
+      </header>
+      <slot name="content"></slot>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    typeOfClosing: {
+      type: String,
+      default: 'none',
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: 'Sample Text',
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.popup {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+}
+.popup__content {
+  position: absolute;
+  top: 20%;
+  left: 45%;
+  text-align: center;
+  background-color: #010b25;
+  color: #fff;
+  border: solid #192847 5px;
+  border-radius: 15px;
+  width: 250px;
+  min-height: 300px;
+}
+.popup__title {
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  background-color: #041d57;
+  padding: 10px 0px;
+  color: #4c8cd0;
+  font-size: 20px;
+}
+.popup__title > h2 {
+  margin: 0;
+}
+</style>
