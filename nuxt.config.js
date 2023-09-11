8d6fd1321b5eec084d55312a98e168c412504b9b
Olga <olga68ko@gmail.com>
2023-09-11 17:10:04 +0300

    deploy

---
+++ nuxt.config.js
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  mode: 'static',
+  router: {
+    base: '/Playvision-test-task/dist/',
+  },
+  devtools: { enabled: true },
+});
